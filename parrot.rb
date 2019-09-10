3b1bbcf261c141cab12205a029bc1c667abbb822
Daniel Cowpertwait <daniel.cowpertwait@gmail.com>
2019-09-10 01:32:45 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot("Squawk!")
-  phrase = "Pretty bird!"
+def parrot(phrase="Squawk!")
+  
   return phrase
 end
